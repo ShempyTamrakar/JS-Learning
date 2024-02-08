41bb77f21c0512c68273e1cc666d9dfaf42bbfb3
ShempyTamrakar <90965501+ShempyTamrakar@users.noreply.github.com>
2024-02-08 08:57:57 +0000

    operation as well link to more understand

--- 01_basics/03_conversionAndOperation.js
+++ 01_basics/03_conversionAndOperation.js
@@ -18,9 +18,9 @@ input value     type        o/p of value
 let isLoggedIn = 1;
 let IsStringIn = "";
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 let IsStringLoggedIn = Boolean(IsStringIn);
-console.log(IsStringLoggedIn);
+// console.log(IsStringLoggedIn);
 
 /*Notes
     1. 1 => true; 0 => false
@@ -30,5 +30,48 @@ console.log(IsStringLoggedIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber); 
-console.log(typeof stringNumber);
+// console.log(stringNumber); 
+// console.log(typeof stringNumber);
+
+/****************Operation*********************** */
+
+let vlaue = 3;
+let negValue = -vlaue
+// console.log(negValue);
+// console.log("addition",2+2);
+// console.log("substration",2-2);
+// console.log("multiplication",2*2);
+// console.log("power",2**3);
+// console.log("modulo",2/3);
+// console.log("division",2%3);
+
+// let str1 = "hello"
+// let str2 = "hitesh"
+
+// let str3 = str1 + str2
+// console.log("concation",str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log((3 + 4) * 5 % 3);
+// console.log(true); // output will be true only
+// console.log(+true); // output will be 1
+// // console.log(true+); // it will give the error
+// console.log(+""); // output will be 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameConter = 100
+gameConter++; 
+++gameConter;
+console.log(gameConter);
+// link to study  1st link for understand the conversion 
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+// second link for understand the prefix and postfix
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment 
